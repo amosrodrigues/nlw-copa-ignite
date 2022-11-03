1a61be79d67293c56fee46e282a01f162a6dd40d
Amós Rodrigues <amos.adm.rh@gmail.com>
2022-11-03 14:30:30 -0300

    server/feat: implement fastify jwt.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
 import { poolRoutes } from './routes/pool'
 import { userRoutes } from './routes/user'
@@ -16,14 +17,14 @@ async function bootstrap() {
     origin: true,
   })
 
+  await fastify.register(jwt, {
+    secret: 'nlwcopa',
+  })
+
   await fastify.register(poolRoutes)
-
   await fastify.register(authRoutes)
-
   await fastify.register(gameRoutes)
-
   await fastify.register(guessRoutes)
-
   await fastify.register(userRoutes)
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
